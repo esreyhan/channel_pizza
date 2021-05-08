c1ec11eae650b03dcf27c448675e008f1f1b899b
Nazlı Tunçer <nazlituncer@MacBook-Pro.local>
2021-05-08 14:58:36 +0300

    Initializing

---
+++ src/main/java/group/channel_pizza/ChannelPizzaApplication.java
@@ -0,0 +1,13 @@
+package group.channel_pizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChannelPizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChannelPizzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/group/channel_pizza/ChannelPizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package group.channel_pizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChannelPizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
